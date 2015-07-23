8c6d86b3bd17d0f8f4ff042824a20b81187a62c4
eriy <freebsderiy@gmail.com>
2015-07-23 09:55:26 +0300

    socket 1.3.6

--- handlers/socket.js
+++ handlers/socket.js
@@ -24,12 +24,30 @@ var Socket = function( server, app ) {
     );
 
     io.adapter( redis({
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
+        /*host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT*/
     }) );
 
     io.use( sharedSession( sessionStore, { autoSave: true } ) );
     io.use( function( socket, next ) {
+        var err;
+
+        if ( ! socket.handshake.session.uId ) {
+            err = new Error({ message: 'unAuthorized', socketId: socket.id });
+            err.status = 403;
+
+            if ( process.env.NODE_ENV === 'development') {
+                console.log('Socket: ', socket.id, ' unAuthorized','\n', 'session: ', socket.handshake);
+            }
+            socket.emit('newMessage', { message: 'unAuthorized', socketId: 'system'});
+
+            return next( err );
+        }
+
+        if ( process.env.NODE_ENV === 'development') {
+            console.log('Socket: ', socket.id, ' isAuthorized, UserId: ', socket.handshake.session.uId);
+        }
+
         next();
     });
 
@@ -37,7 +55,7 @@ var Socket = function( server, app ) {
         socket.emit('newMessage', { message: 'you are connected', socketId: socket.id });
         console.log(
             'Socket connected: ', socket.id, '\n',
-            'UserID: ', socket.handshake.session.user
+            'session: ', socket.handshake.session
         );
 
         socket.on('newMessage', function( data ) {
@@ -46,6 +64,18 @@ var Socket = function( server, app ) {
                 socket.broadcast.to( data.room ).emit('newMessage', { message: data.message, socketId: socket.handshake.session.user ||socket.id } );
                 console.log( 'New message\n', 'from: ', socket.id, '\n', 'to room: ', data.room );
             } else {
+                socket.emit('newMessage', { message: 'your test msg resived', data: data, socketId:socket.handshake.session.user || socket.id });
+            }
+
+        });
+
+        socket.on('user:login', function( data ) {
+            if ( data.room ) {
+                console.log( data.room );
+                socket.broadcast.to( data.room ).emit('newMessage', { message: data.message, socketId: socket.handshake.session.user ||socket.id } );
+                console.log( 'New message\n', 'from: ', socket.id, '\n', 'to room: ', data.room );
+            } else {
+                console.log('Socket IN: ', socket.id, ' Data: ', data);
                 socket.emit('newMessage', { message: 'your test msg resived', data: data, socketId:socket.handshake.session.user || socket.id });
             }
 
